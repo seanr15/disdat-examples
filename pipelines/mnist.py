ea96f74cd303c0f3959b2f17bff5b94f839ebbe6
Ken Yocum <kyocum@gmail.com>
2021-06-22 00:11:00 -0600

    updated tensorflow example to latest

--- pipelines/mnist.py
+++ pipelines/mnist.py
@@ -16,159 +16,97 @@ from __future__ import print_function
 #
 
 from builtins import range
+
+import keras.models
+import luigi
 from disdat.pipe import PipeTask
 import disdat.api as api
 import tensorflow as tf
-from tensorflow.contrib.learn.python.learn.datasets import mnist as tf_mnist
-from tensorflow.contrib.learn.python.learn.datasets import base
-from tensorflow.python.framework import dtypes
-from tensorflow.python.platform import gfile
+import tensorflow_datasets as tfds
 import os
+from os import walk
+
 
 """
 MNIST
 
-This is a direct adaptation of https://www.tensorflow.org/get_started/mnist/pros
+This is an adaptation of https://www.tensorflow.org/datasets/keras_example
 
-It also takes code from
-https://github.com/tensorflow/tensorflow/blob/r1.4/tensorflow/examples/how_tos/reading_data/convert_to_records.py
-
-This example shows how you can save TensorFlow outputs into bundles.
-
-Pre Execution:
-$export PYTHONPATH=$DISDAT_HOME/disdat/examples/pipelines
-$cd ${DISDAT_HOME}/examples; pip install -e .
-$dsdt context examples; dsdt switch examples
+This example shows how you can save TensorFlow datasets and Keras models in bundles and then
+ use them as inputs into successive tasks. 
 
 Execution:
 $python ./mnist.py
 or:
-$dsdt apply - - mnist.Evaluate
+$dsdt apply pipelines.mnist.Evaluate
 
 """
 
-
-# CVDF mirror of http://yann.lecun.com/exdb/mnist/
-DEFAULT_SOURCE_URL = 'https://storage.googleapis.com/cvdf-datasets/mnist/'
+# Note: Disdat interprets local and s3 file paths inside of bundles.  Sometimes we don't want that, e.g., when
+# we are saving directories.  We add a prefix when saving such paths in bundles.
+NOLINK_PREFIX = "nolink://"
 
 """-----------------------------"""
 """ TensorFlow Helper Functions """
 """-----------------------------"""
 
-
-def _int64_feature(value):
-    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
-
-
-def _bytes_feature(value):
-    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+def list_files(dir:str) -> list:
+    f = []
+    for (dirpath, dirnames, filenames) in walk(dir):
+        f.extend([os.path.join(dirpath,f) for f in filenames])
+    return f
 
 
-def dl_data_sets(train_dir, source_url=DEFAULT_SOURCE_URL):
-    """ Modified version of tensorflow/tensorflow/contrib/learn/python/learn/datasets/mnist.py
-    Returns:
-        (dict): dataset name to local file path
+def normalize_img(image, label):
+    """Normalizes images: `uint8` -> `float32`."""
+    return tf.cast(image, tf.float32) / 255., label
+
+
+def get_mnist_tfds(tfds_name, data_dir):
     """
+    Use TFDS to extract a dataset from that stared at the data_dir.
+    Args:
+        tfds_name (str): Name of the tfds
+        data_dir (str): The local file path where we stored the tfds
 
-    if not source_url:  # empty string check
-        source_url = DEFAULT_SOURCE_URL
-
-    TRAIN_IMAGES = 'train-images-idx3-ubyte.gz'
-    TRAIN_LABELS = 'train-labels-idx1-ubyte.gz'
-    TEST_IMAGES = 't10k-images-idx3-ubyte.gz'
-    TEST_LABELS = 't10k-labels-idx1-ubyte.gz'
-
-    local_files = {'train-images': [base.maybe_download(TRAIN_IMAGES, train_dir, source_url + TRAIN_IMAGES)]}
-
-    local_files['train-labels'] = [base.maybe_download(TRAIN_LABELS, train_dir,
-                                   source_url + TRAIN_LABELS)]
-
-    local_files['t10k-images'] = [base.maybe_download(TEST_IMAGES, train_dir,
-                                   source_url + TEST_IMAGES)]
-
-    local_files['t10k-labels'] = [base.maybe_download(TEST_LABELS, train_dir,
-                                   source_url + TEST_LABELS)]
-
-    return local_files
-
-
-def convert_to_data_sets(data_gzs,
-                         one_hot=False,
-                         dtype=dtypes.float32,
-                         reshape=True,
-                         validation_size=5000,
-                         seed=None):
-    """ Modified version of tensorflow/tensorflow/contrib/learn/python/learn/datasets/mnist.py """
-
-    with gfile.Open(data_gzs['train-images'][0], 'rb') as f:
-        train_images = tf_mnist.extract_images(f)
-
-    with gfile.Open(data_gzs['train-labels'][0], 'rb') as f:
-        train_labels = tf_mnist.extract_labels(f, one_hot=one_hot)
-
-    with gfile.Open(data_gzs['t10k-images'][0], 'rb') as f:
-        test_images = tf_mnist.extract_images(f)
-
-    with gfile.Open(data_gzs['t10k-labels'][0], 'rb') as f:
-        test_labels = tf_mnist.extract_labels(f, one_hot=one_hot)
-
-    if not 0 <= validation_size <= len(train_images):
-        raise ValueError('Validation size should be between 0 and {}. Received: {}.'.format(len(train_images),
-                                                                                            validation_size))
-
-    validation_images = train_images[:validation_size]
-    validation_labels = train_labels[:validation_size]
-    train_images = train_images[validation_size:]
-    train_labels = train_labels[validation_size:]
-
-    options = dict(dtype=dtype, reshape=reshape, seed=seed)
-
-    train = tf_mnist.DataSet(train_images, train_labels, **options)
-    validation = tf_mnist.DataSet(validation_images, validation_labels, **options)
-    test = tf_mnist.DataSet(test_images, test_labels, **options)
-
-    return base.Datasets(train=train, validation=validation, test=test)
-
-
-def make_model():
-    """ Create single layer NN  -- softmax output """
-
-    # Add to the default tf graph
-    x = tf.placeholder(tf.float32, shape=[None, 784])
-    y_ = tf.placeholder(tf.float32, shape=[None, 10])
-
-    W = tf.get_variable("W", [784,10], initializer=tf.zeros_initializer)
-    b = tf.get_variable("b", [10], initializer = tf.zeros_initializer)
-
-    y = tf.matmul(x, W, name="linmatmul") + b
-
-    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-
-    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-
-    return train_step, cross_entropy, x, y_, y
-
+    Returns:
+        (ds_info, dataset)
+    """
+    builder = tfds.builder(tfds_name, data_dir=data_dir)
+    ds_info = builder.info
+    dataset = builder.as_dataset(split=['train','test'], shuffle_files=True, as_supervised=True)
+    return (ds_info, dataset)
 
 """-----------------------------"""
 """      Disdat Pipe Tasks      """
 """-----------------------------"""
 
-
-class GetDataGz(PipeTask):
+class GetTFDS(PipeTask):
     """ Pipe Task 1
-    Get the data for MNIST as raw gz files
+    Get the data for MNIST using TF dataset API
+    This is different than the standard TFDS, which uses one place
+    to store you TF datasets (typically under your home dir).   Here we
+    use Disdat to place each tfds inside a bundle, and give the bundle the
+    name of the dataset.  We also store the version in a tag.
     """
+    name = luigi.Parameter(default="mnist")
+
     def pipe_requires(self):
         """ Simply name the output of this PipeTask """
-        self.set_bundle_name("MNIST.data.gz")
+        self.set_bundle_name(self.name)
 
     def pipe_run(self):
         """
+        Simply download_and_prepare to the bundle directory directly.
+        Return the list of files in the bundle so Disdat manages them.
         Returns:
-            (dict): Dictionary of name, path pairs
+            (dict): Return the directory
         """
-        files = dl_data_sets(self.get_output_dir())
-        return files
+        data_dir = self.get_output_dir()
+        builder = tfds.builder(self.name, data_dir=data_dir)
+        builder.download_and_prepare()
+        files = list_files(data_dir)
+        return {'data_dir':[NOLINK_PREFIX+data_dir], 'files': files}
 
 
 class Train(PipeTask):
@@ -177,87 +115,92 @@ class Train(PipeTask):
     Returns:
         (dict): all the model files in 'save_files' and the name of the dir in 'save_dir'
     """
+    name = luigi.Parameter(default='mnist')
 
     def pipe_requires(self):
         """ Depend on the gzip files being downloaded  """
-        self.add_dependency("input_gzs", GetDataGz, {})
-        self.set_bundle_name("MNIST.trained")
+        self.add_dependency("mnist_tfds", GetTFDS, {'name': self.name})
+        self.set_bundle_name("mnist-trained")
 
-    def pipe_run(self, input_gzs=None):
+    def pipe_run(self, mnist_tfds=None):
         """        """
-
         print("Beginning training . . . ")
 
-        # Bring the data in as an mnist tutorial dataset
-        mnist = convert_to_data_sets(input_gzs, one_hot=True)
+        data_dir = mnist_tfds['data_dir'][0][len(NOLINK_PREFIX):]
+        ds_info, mnist = get_mnist_tfds(self.name, data_dir)
 
-        g = tf.Graph()
-        with g.as_default():
-            train_step, cross_entropy, x, y_, _ = make_model()
-            saver = tf.train.Saver()
+        ds_train = mnist[0]
+        ds_train = ds_train.map(normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        ds_train = ds_train.cache()
+        ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+        ds_train = ds_train.batch(128)
+        ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
 
-        with tf.Session(graph=g) as sess:
-            sess.run(tf.global_variables_initializer())
-
-            for _ in range(1000):
-                batch = mnist.train.next_batch(100)
-                train_step.run(feed_dict={x: batch[0], y_: batch[1]})
-
-            save_dir = os.path.join(self.get_output_dir(), 'MNIST')
-            saver.save(sess, os.path.join(save_dir, 'MNIST_tf_model'))
-
+        model = tf.keras.models.Sequential([
+            tf.keras.layers.Flatten(input_shape=(28, 28)),
+            tf.keras.layers.Dense(128,activation='relu'),
+            tf.keras.layers.Dense(10)
+        ])
+        model.compile(
+            optimizer=tf.keras.optimizers.Adam(0.001),
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+        )
+        model.fit(
+            ds_train,
+            epochs=6
+        )
+        model_output_dir = self.create_output_dir("keras_model")
+        model.save(model_output_dir)
         print("End training.")
 
-        # Note 1: When returning a dictionary, disdat requires you to use a sequence
-        # in the value.
-
-        # Note 2: You can return file paths, directories, or luigi.LocalTarget.
-        # If a directory, Disdat takes all files directly under the directory.
-
-        return {'save_files': [save_dir], 'model_name': ['MNIST_tf_model']}
+        # Note 1: When returning a dictionary, disdat requires a sequence for each value
+        # Note 2: You can return file paths or luigi.LocalTarget.
+        # Returning directories results in an error.
+        files = list_files(model_output_dir)
+        return {'output_dir': [os.path.join(NOLINK_PREFIX+model_output_dir)],
+                'files': files}
 
 
 class Evaluate(PipeTask):
     """ Pipe Task 2
     Evaluate model from Train
     """
+    name = luigi.Parameter(default='mnist')
+
     def pipe_requires(self):
         """ """
-        self.add_dependency("model", Train, {})
-        self.add_dependency("input_gzs", GetDataGz, {})
-        self.set_bundle_name("MNIST.eval")
+        self.add_dependency("model", Train, {'name': self.name})
+        self.add_dependency("mnist_tfds", GetTFDS, {'name': self.name})
+        self.set_bundle_name("mnist-evaluation")
 
-    def pipe_run(self, model=None, input_gzs=None):
+    def pipe_run(self, model=None, mnist_tfds=None):
         """
         Args:
-            pipeline_input:
-            model:
-
-        Returns:
-
+            model (bundle.data): Dictionary with a key for output_dir, and a set of files
+            mnist_tfds (bundle.data): Dictionary with a key for output_dir, and a set of files
         """
 
         print ("Begin evaluation . . . ")
+        data_dir = mnist_tfds['data_dir'][0][len(NOLINK_PREFIX):]
+        ds_info, mnist = get_mnist_tfds(self.name, data_dir)
 
-        mnist = convert_to_data_sets(input_gzs, one_hot=True)
+        ds_test = mnist[1]
+        ds_test = ds_test.map(normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        ds_test = ds_test.batch(128)
+        ds_test = ds_test.cache()
+        ds_test = ds_test.prefetch(tf.data.experimental.AUTOTUNE)
 
-        g = tf.Graph()
-        with g.as_default():
-            train_step, cross_entropy, x, y_, y = make_model()
+        # Restore the model
+        model_output_dir = model['output_dir'][0][len(NOLINK_PREFIX):]
+        model = keras.models.load_model(model_output_dir)
 
-        with tf.Session(graph=g) as sess:
-            save_dir = os.path.dirname(model['save_files'][0])
-            tf.train.Saver().restore(sess, tf.train.latest_checkpoint(save_dir))
-            correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-            report = accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels})
-            print(report)
-
+        results = model.evaluate(ds_test)
+        print("----->>>>> test loss, test acc:", results)
         print ("End evaluation.")
 
-        return report
+        return results
 
 
 if __name__ == "__main__":
-    print ("Using Disdat API to run the pipeline")
-    api.apply('examples', Evaluate)
+    api.apply('example-context', Evaluate)
